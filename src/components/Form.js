8cb9de447d7353ebe350d7fb9d5a00fa2f503c28
Zilliana Sousa <33105906+zilliana123@users.noreply.github.com>
2025-08-06 00:52:24 -0300

    feat: mascara do cpf

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,9 +5,21 @@ function Form() {
   // Estado para armazenar os valores dos campos
   const [nome, setNome] = useState("");
   const [email, setEmail] = useState("");
-  const [cpf, setCpf] = useState(""); // Novo estado para CPF
-  const [salaDestino, setSalaDestino] = useState(""); // Novo estado para sala de destino
-  const [dataNascimento, setDataNascimento] = useState(""); // Novo estado para data de nascimento
+  const [cpf, setCpf] = useState("");
+  const [salaDestino, setSalaDestino] = useState("");
+  const [dataNascimento, setDataNascimento] = useState("");
+
+  //Máscara do CPF
+  const formatCpf = (value) => {
+    // Remove tudo que não é número
+    const numericValue = value.replace(/\D/g, "");
+
+    // Aplica a máscara
+    return numericValue
+      .replace(/(\d{3})(\d)/, "$1.$2")
+      .replace(/(\d{3})(\d)/, "$1.$2")
+      .replace(/(\d{3})(\d{1,2})$/, "$1-$2");
+  };
 
   // Função para lidar com o envio do formulário
   const handleSubmit = (e) => {
@@ -20,26 +32,46 @@ function Form() {
     <form onSubmit={handleSubmit} className="AppForm">
       <div className="FormItem">
         <label>Nome: </label>
-        <input type="text" value={nome} />
+        <input
+          type="text"
+          value={nome}
+          onChange={(e) => setNome(e.target.value)}
+        />
       </div>
       <div className="FormItem">
         <label>Email: </label>
-        <input type="email" value={email} />
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </div>
       {/* Novo campo de CPF */}
       <div className="FormItem">
         <label>CPF: </label>
-        <input type="text" value={cpf} />
+        <input
+          type="text"
+          value={cpf}
+          onChange={(e) => setCpf(formatCpf(e.target.value))}
+        />
       </div>
       {/* Novo campo de sala de destino */}
       <div className="FormItem">
         <label>Sala de Destino: </label>
-        <input type="text" value={salaDestino} />
+        <input
+          type="text"
+          value={salaDestino}
+          onChange={(e) => setSalaDestino(e.target.value)}
+        />
       </div>
       {/* Novo campo de data de nascimento */}
       <div className="FormItem">
         <label>Data de Nascimento: </label>
-        <input type="date" value={dataNascimento} />
+        <input
+          type="date"
+          value={dataNascimento}
+          onChange={(e) => setDataNascimento(e.target.value)}
+        />
       </div>
       <button type="submit" className="FormButton">
         Enviar
